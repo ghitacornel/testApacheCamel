234a39702193df28f3fc361e75df885b071823ac
ghita <ghita.cornel@gmail.com>
2023-04-24 10:01:29 +0300

    better

--- timer-jpa-rest-jms-jpa/src/main/java/camel/test/ExternalVoucherService.java
+++ timer-jpa-rest-jms-jpa/src/main/java/camel/test/ExternalVoucherService.java
@@ -1,6 +1,7 @@
 package camel.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +14,10 @@ public class ExternalVoucherService {
     private final Random random = new Random();
 
     @GetMapping(value = "{id}")
-    public Integer getPercentage() {
+    public Integer getPercentage(@PathVariable(name = "id") Integer id) {
         int i = random.nextInt();
         if (i > 80) {
-            throw new RuntimeException("simulate error");
+            throw new RuntimeException("simulate error for order id " + id);
         }
         return random.nextInt(100);
     }
