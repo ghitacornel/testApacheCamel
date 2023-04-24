a57792eea5c280722c0ae28866849eba7e18e16d
ghita <ghita.cornel@gmail.com>
2023-04-24 09:30:59 +0300

    better

--- timer-jpa-rest-jms-jpa/src/main/java/camel/route/steps/CallVoucherForPercentageProcessor.java
+++ timer-jpa-rest-jms-jpa/src/main/java/camel/route/steps/CallVoucherForPercentageProcessor.java
@@ -19,13 +19,16 @@ public class CallVoucherForPercentageProcessor implements Processor {
     private final OrderRepository orderRepository;
     private final RestTemplate restTemplate = new RestTemplate();
 
-    // @Transactional no transactional here multiple steps with possible failing conditions
+    // @Transactional no transactional here => multiple steps with possible failing conditions
     @Override
     public void process(Exchange exchange) throws Exception {
+
+        // step 1
         Order order = exchange.getMessage().getBody(Order.class);
         order.setStatus(OrderStatus.VOUCHER_PERCENTAGE);
         orderRepository.save(order);
 
+        // step 2
         ResponseEntity<Integer> response;
         try {
             response = restTemplate.getForEntity("http://localhost:8080/voucher/{id}", Integer.class, order.getId());
@@ -34,6 +37,7 @@ public class CallVoucherForPercentageProcessor implements Processor {
             return;
         }
 
+        // step 3
         order.setPercentageVoucherReduction(response.getBody());
         order.setStatus(OrderStatus.VOUCHER_PERCENTAGE_COMPLETED);
         orderRepository.save(order);
