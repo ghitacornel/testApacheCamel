f212642c1e79561cfedce42eb03edc0e87980d3b
ghita <ghita.cornel@gmail.com>
2023-04-24 10:32:12 +0300

    better

--- timer-jpa-rest-jms-jpa/src/main/java/camel/test/ExternalVoucherService.java
+++ timer-jpa-rest-jms-jpa/src/main/java/camel/test/ExternalVoucherService.java
@@ -16,7 +16,7 @@ public class ExternalVoucherService {
     @GetMapping(value = "{id}")
     public Integer getPercentage(@PathVariable(name = "id") Integer id) {
         int i = random.nextInt(100);
-        if (i > 90) {
+        if (i > 10) {
             throw new SimulatorException("simulate error for order id " + id);
         }
         return random.nextInt(100);
