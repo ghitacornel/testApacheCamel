7f794c105ee2053f3b5a05a2454009d50cc85d7c
ghita <ghita.cornel@gmail.com>
2023-08-26 23:56:57 +0300

    better

---
+++ multicast/src/main/java/camel/MulticastMain.java
@@ -0,0 +1,29 @@
+package camel;
+
+import camel.route.MulticastParallelRoute;
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class MulticastMain {
+
+    public static void main(String[] args) throws Exception {
+        try (CamelContext camelContext = new DefaultCamelContext()) {
+            camelContext.addRoutes(new MulticastParallelRoute());
+            camelContext.start();
+            {
+                // nothing happens till we don't initiate a startup message
+                // initiate a startup message
+                // can be a dummy one
+                camelContext.createProducerTemplate().sendBody("direct:start", "This is a dummy startup message");
+            }
+
+            {
+                // nothing happens till we don't initiate a startup message
+                // initiate a startup message
+                // can be a dummy one
+                camelContext.createProducerTemplate().sendBody("direct:start", "This is a dummy startup message");
+            }
+
+        }
+    }
+}
